416013b8063145e38476bea79af959003d609ebc
ajcase <ajcase@gmail.com>
2020-05-08 10:06:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ app.get('/oauth/callback', passport.authenticate('openidconnect', {
 // revoke the access_token at IBM Cloud Identity
 app.get('/logout', function(req, res){
   
-  request.post(`${OIDC_TOKEN_URI}/revoke`, {
+  request.post(`${OIDC_BASE_URI}/revoke`, {
     'form':{
       'client_id': process.env.OIDC_CLIENT_ID,
       'client_secret': process.env.OIDC_CLIENT_SECRET,
